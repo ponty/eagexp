433007371c9f0d66ecf118ee8b53920502ab4606
ponty <titi@pince.(none)>
2011-04-28 19:00:33 +0200

    new options

--- eagexp/image.py
+++ eagexp/image.py
@@ -1,7 +1,10 @@
+import Image
 from eagexp import __version__
 from eagexp.exp import export_eagle
 from entrypoint2 import entrypoint
+import ImageOps
 import logging
+import tempfile
 
 log = logging.getLogger(__name__)
 log.debug('version=' + __version__)
@@ -9,7 +12,7 @@ log.debug('version=' + __version__)
 
 
 @entrypoint
-def export_image(input, output, timeout=20, palette='white', resolution=150, showgui=False):
+def export_image(input, output, timeout=20, palette='white', resolution=150, layers=None, command=None, mirror=False, showgui=False):
     '''    
     Exporting eagle .sch or .brd file into image file.
     GUI is not displayed if PyVirtualDisplay is installed.
@@ -42,6 +45,9 @@ def export_image(input, output, timeout=20, palette='white', resolution=150, sho
     :param resolution: image resolution in dpi (50..2400)
     :param timeout: operation is canceled after this timeout (sec) 
     :param showgui: eagle GUI is displayed
+    :param layers: list, layers to be displayed ['top','pads'] 
+    :param command: string, direct eagle command
+    :param mirror: Bool
     :rtype: None
     '''
         
@@ -51,12 +57,33 @@ def export_image(input, output, timeout=20, palette='white', resolution=150, sho
     if palette == 'none':
         palette = None
             
+    cmds = []
     if palette is not None:
-        commands = ['SET PALETTE {palette};'.format(palette=palette)]
+        cmds += ['SET PALETTE {palette}'.format(palette=palette)]
+
+    if layers is not None:
+        cmds += ['DISPLAY NONE ' + ' '.join(layers)]
+        
+    if command is not None:
+        cmds += [command]
+
+    if mirror:
+        f = tempfile.NamedTemporaryFile(suffix='.png', prefix='eagexp_')
+        fout = f.name
     else:
-        commands=[]
-    export_eagle(input=input, output=output, output_type='image',
-                 timeout=timeout, commands=commands, showgui=showgui, 
+        fout = output
+
+    export_eagle(input=input, output=fout, output_type='image',
+                 timeout=timeout, commands=cmds, showgui=showgui,
                  resolution=resolution)
     
+
+    if mirror:
+        im = Image.open(fout)
+        # save dpi info
+        info=im.info
+        im = ImageOps.mirror(im)
+        im.save(output, **info)
+
+
 
