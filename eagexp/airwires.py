a4fe7caf16acc7b11155b6f74d64b1d31b61d2e0
ponty <titi@pince.(none)>
2011-06-10 21:04:54 +0200

    new:airwires

---
+++ eagexp/airwires.py
@@ -0,0 +1,53 @@
+from eagexp.cmd import command_eagle
+from path import path
+import tempfile
+
+
+ulp_templ = r'''
+int count=0;
+
+if (board) {
+    board(B) {
+        B.signals(S) {
+            S.wires(W) {
+                if (W.layer == 19) {
+                    count++;
+                }
+            }
+        }
+    }
+  output("FILE_NAME", "wt")
+  {
+    printf("%d\n", count);
+  };
+}
+'''
+
+def airwires(board, showgui=0):
+    'search for airwires in eagle board'
+    board = path(board).abspath()
+    
+    file_out = tempfile.NamedTemporaryFile(suffix='.txt', delete=0)
+    file_out.close()
+
+    ulp = ulp_templ.replace('FILE_NAME', file_out.name)
+    
+    file_ulp = tempfile.NamedTemporaryFile(suffix='.ulp', delete=0)
+    file_ulp.write(ulp)
+    file_ulp.close()
+    
+    commands = [
+              'run ' + file_ulp.name,
+              'quit',
+              ]
+    command_eagle(board, commands=commands, showgui=showgui)
+    
+    n = int(path(file_out.name).text())
+    
+    path(file_out.name).remove()
+    path(file_ulp.name).remove()
+    
+    return n
+    
+    
+    
