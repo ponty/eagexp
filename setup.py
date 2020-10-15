96735b7781ccb182f64f38039c97356d26b398a5
ponty <ponty@home>
2020-10-15 08:22:39 +0200

    setup.py: set long_description_content_type

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
     classifiers=classifiers,
     keywords="eagle",
     author="ponty",
